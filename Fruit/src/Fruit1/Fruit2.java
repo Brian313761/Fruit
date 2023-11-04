2fdcb848ec0119eaaf4e80f8d7dc0d597ff46585
Brian313761 <140124219+Brian313761@users.noreply.github.com>
2023-11-03 22:26:27 -0400

    Add files via upload

---
+++ Fruit/src/Fruit1/Fruit2.java
@@ -0,0 +1,31 @@
+package Fruit1;
+
+/*import java.util.ArrayList;*/
+
+public class Fruit2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	
+	/*ArrayList<String> Fruit=new ArrayList<>();
+
+	Fruit.add("apple");
+	Fruit.add("banana");
+	Fruit.add("kiwi");
+	Fruit.add("pinapple");
+	Fruit.add("strawberry");
+	/*Fruit.remove(2);
+	Fruit.set(0,"blueberry");*/
+	/*Fruit.add(3,"banana");
+	
+	System.out.println(Fruit);*/ 
+
+		
+		System.out.println(-5+8*6);
+		System.out.println((55+9)%9);
+		System.out.println(20+3*5/8);
+		System.out.println(5+15/3*2-8%3);
+
+	}
+}
